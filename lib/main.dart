9fee1a4739025d26bc70ec93c83802390e8a55cc
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-12-25 13:52:30 +0530

    change file structer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fuel_app/providers/user_providers.dart';
-import 'package:fuel_app/routes/route.dart';
+import 'package:fuel_app/route.dart';
 import 'package:fuel_app/screens/signup_screens.dart';
-import 'package:fuel_app/screens/splashScreen.dart';
+import 'package:fuel_app/splashScreen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
